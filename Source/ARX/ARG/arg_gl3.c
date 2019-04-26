7b6eb67f207891a1b0bd443e2a75545022fce0ae
Guillaume Martres <smarter@ubuntu.com>
2019-04-26 15:41:31 +0200

    Fix loading opengl functions under Windows

--- Source/ARX/ARG/arg_gl3.c
+++ Source/ARX/ARG/arg_gl3.c
@@ -79,9 +79,24 @@
 #  define GL_UNSIGNED_SHORT_8_8_REV_APPLE 0x85BB
 #endif
 
+void *GetAnyGLFuncAddress(const char *name)
+{
+  void *p = (void *)wglGetProcAddress(name);
+  if(p == 0 ||
+    (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) ||
+    (p == (void*)-1) )
+  {
+    HMODULE module = LoadLibraryA("opengl32.dll");
+    p = (void *)GetProcAddress(module, name);
+  }
+
+  return p;
+}
+
 // On Windows, all OpenGL v3 and later API must be dynamically resolved against the actual driver
 #if defined(_WIN32)
-    # define ARGL_GET_PROC_ADDRESS wglGetProcAddress
+//    # define ARGL_GET_PROC_ADDRESS wglGetProcAddress
+    # define ARGL_GET_PROC_ADDRESS GetAnyGLFuncAddress
     static PFNGLBINDTEXTUREPROC glBindTexture = NULL;
     static PFNGLDELETETEXTURESPROC glDeleteTextures = NULL;
     static PFNGLGENTEXTURESPROC glGenTextures = NULL;
